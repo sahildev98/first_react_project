389276ad9ca503a540b718c5422a6bd2b2c2f869
sahil <sahil.98@hotmail.com>
2023-09-11 00:58:26 +0100

    Refactored function name and fixed unreachable code issue by placing the code next to return stateement

--- react-lessons/src/navbar.js
+++ react-lessons/src/navbar.js
@@ -1,6 +1,5 @@
-export default function navBar(){
-    return
-        <nav>
+export default function Navbar(){
+    return <nav>
           <li><a href='Projects.html'>Projects</a></li>
           <li><a href='about.html'>About</a></li>
           <li><a href='Projects.html'>Contact</a></li>
