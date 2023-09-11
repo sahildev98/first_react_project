1e0db2ec424d2add4bf71c4b44eb3dce6bedcd2c
sahildev98 <sahil.98@hotmail.com>
2023-09-11 14:26:59 +0100

    Included ul tag on to navbar

--- react-lessons/src/navbar.js
+++ react-lessons/src/navbar.js
@@ -1,9 +1,11 @@
 export default function Navbar(){
     return <nav className="nav">
           <h1>SA clothing</h1>
-          <li><a href='Projects.html'>Projects</a></li>
-          <li><a href='about.html'>About</a></li>
-          <li><a href='Projects.html'>Contact</a></li>
+          <ul>
+            <li><a href='Projects.html'>Projects</a></li>
+            <li><a href='about.html'>About</a></li>
+            <li><a href='Projects.html'>Contact</a></li>
+          </ul>
         </nav>
     
 }
