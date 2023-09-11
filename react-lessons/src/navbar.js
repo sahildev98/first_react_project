4ab81832059101c2489016f37b352977ce2f0e93
sahildev98 <sahil.98@hotmail.com>
2023-09-11 14:24:30 +0100

    Included a h1 title to navbar

--- react-lessons/src/navbar.js
+++ react-lessons/src/navbar.js
@@ -1,5 +1,6 @@
 export default function Navbar(){
     return <nav className="nav">
+          <h1>SA clothing</h1>
           <li><a href='Projects.html'>Projects</a></li>
           <li><a href='about.html'>About</a></li>
           <li><a href='Projects.html'>Contact</a></li>
