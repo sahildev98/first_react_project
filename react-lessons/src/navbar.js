e2af144280d8bea81f1483c01c523da8b1f65545
sahil <sahil.98@hotmail.com>
2023-09-12 18:02:03 +0100

    Added empty nodes to behave as a parent container and imported header into the navbar

--- react-lessons/src/navbar.js
+++ react-lessons/src/navbar.js
@@ -1,11 +1,14 @@
+import Header from './header';
 export default function Navbar(){
-    return <nav className="nav">
-          <h1>SA clothing</h1>
+    return <>
+    <Header/>
+    <nav className="nav">
           <ul>
             <li><a href='Projects.html'>Projects</a></li>
             <li><a href='about.html'>About</a></li>
             <li><a href='Projects.html'>Contact</a></li>
           </ul>
         </nav>
+        </>
     
 }
